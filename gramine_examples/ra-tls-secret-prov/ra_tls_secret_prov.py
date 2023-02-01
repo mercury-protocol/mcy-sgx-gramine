f1c193d4f20848f332c7fef2e85399fa376e9890
mercury <founders@mercuryprotocol.io>
2023-02-01 08:00:52 +0100

    add gramine sgx tools + update makefile

--- gramine_examples/ra-tls-secret-prov/ra_tls_secret_prov.py
+++ gramine_examples/ra-tls-secret-prov/ra_tls_secret_prov.py
@@ -1,9 +1,10 @@
 import os
 from gramine_examples.secrets import *  # not included in git
 
+os.chdir("ra-tls-secret-prov")
+
 
 def perform_commands(commands: list):
-    os.chdir("ra-tls-secret-prov")
     for cmd in commands:
         os.system(cmd)
 
@@ -12,10 +13,11 @@ def epid(linkable: bool = False):
     spid = SPID_L if linkable else SPID_U
     primary_key = IAS_PRIMARY_KEY_L if linkable else IAS_PRIMARY_KEY_U
     secondary_key = IAS_SECONDARY_KEY_L if linkable else IAS_SECONDARY_KEY_U
-    commands = [
+    commands_build = [
         "make clean",
-        f"make app epid RA_TYPE=epid RA_CLIENT_SPID={spid} RA_CLIENT_LINKABLE={int(linkable)}",
-        "cd secret_prov_pf",
+        f"make app epid RA_TYPE=epid RA_CLIENT_SPID={spid} RA_CLIENT_LINKABLE={int(linkable)}"
+    ]
+    commands_run = [
         "RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE=1",  # WARNING: Don't use this option in production!
         "RA_TLS_ALLOW_OUTDATED_TCB_INSECURE=1",  # WARNING: Don't use this option in production!
         f"A_TLS_EPID_API_KEY={primary_key}",
@@ -24,14 +26,17 @@ def epid(linkable: bool = False):
         "kill %%"
     ]
 
-    perform_commands(commands)
+    perform_commands(commands_build)
+    os.chdir("secret_prov_minimal")
+    perform_commands(commands_run)
 
 
 def dcap():
-    commands = [
+    commands_build = [
         "make clean",
-        "make app dcap RA_TYPE=dcap",
-        "cd secret_prov_pf",
+        "make app dcap RA_TYPE=dcap"
+    ]
+    commands_run = [
         "RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE=1",  # WARNING: Don't use this option in production!
         "RA_TLS_ALLOW_OUTDATED_TCB_INSECURE=1",  # WARNING: Don't use this option in production!
         "./server_dcap wrap_key &",
@@ -39,10 +44,12 @@ def dcap():
         "kill %%"
     ]
 
-    perform_commands(commands)
+    perform_commands(commands_build)
+    os.chdir("secret_prov_minimal")
+    perform_commands(commands_run)
 
 
 if __name__ == "__main__":
     """Choose which type of RA to use by commenting the other."""
-    epid(linkable=False)
+    epid(linkable=True)
     # dcap()
