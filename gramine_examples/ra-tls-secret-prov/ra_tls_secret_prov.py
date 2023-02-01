4e745118a462b36f0b7aebb49b5746ade78cea05
mercury <founders@mercuryprotocol.io>
2023-02-01 20:18:01 +0100

    add parameters

--- gramine_examples/ra-tls-secret-prov/ra_tls_secret_prov.py
+++ gramine_examples/ra-tls-secret-prov/ra_tls_secret_prov.py
@@ -1,18 +1,26 @@
 import os
 from gramine_examples.secrets import *  # not included in git
 
+
+MINIMAL = "secret_prov_minimal"
+NORMAL = "secret_prov"
+FULL = "secret_prov_pf"
+
 os.chdir("ra-tls-secret-prov")
 
 
 def perform_commands(commands: list):
+    cmd_color = '\033[96m'
+    color_end = '\033[0m'
     for cmd in commands:
+        print()
+        print(f"{cmd_color}{cmd}{color_end}")
         os.system(cmd)
 
 
-def epid(linkable: bool = False):
+def epid(level: str = MINIMAL, linkable: bool = False):
     spid = SPID_L if linkable else SPID_U
-    primary_key = IAS_PRIMARY_KEY_L if linkable else IAS_PRIMARY_KEY_U
-    secondary_key = IAS_SECONDARY_KEY_L if linkable else IAS_SECONDARY_KEY_U
+    ias_api_key = IAS_PRIMARY_KEY_L if linkable else IAS_PRIMARY_KEY_U
     commands_build = [
         "make clean",
         f"make app epid RA_TYPE=epid RA_CLIENT_SPID={spid} RA_CLIENT_LINKABLE={int(linkable)}"
@@ -20,18 +28,18 @@ def epid(linkable: bool = False):
     commands_run = [
         "RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE=1",  # WARNING: Don't use this option in production!
         "RA_TLS_ALLOW_OUTDATED_TCB_INSECURE=1",  # WARNING: Don't use this option in production!
-        f"A_TLS_EPID_API_KEY={primary_key}",
-        "./server_epid wrap_key &",
+        f"A_TLS_EPID_API_KEY={ias_api_key}",
+        "./server_epid wrap_key &"
         "gramine-sgx ./client",
         "kill %%"
     ]
 
     perform_commands(commands_build)
-    os.chdir("secret_prov_minimal")
+    os.chdir(level)
     perform_commands(commands_run)
 
 
-def dcap():
+def dcap(level: str = MINIMAL):
     commands_build = [
         "make clean",
         "make app dcap RA_TYPE=dcap"
@@ -45,11 +53,11 @@ def dcap():
     ]
 
     perform_commands(commands_build)
-    os.chdir("secret_prov_minimal")
+    os.chdir(level)
     perform_commands(commands_run)
 
 
 if __name__ == "__main__":
     """Choose which type of RA to use by commenting the other."""
-    epid(linkable=True)
-    # dcap()
+    # epid(level=FULL, linkable=False)
+    dcap(level=FULL)
