bb32a02ae05aec53943c0ade5218726ab144f549
mercury <founders@mercuryprotocol.io>
2023-02-11 18:59:34 +0100

    wip

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 REPORT_PATH = "/dev/attestation/user_report_data"
 QUOTE_PATH = "/dev/attestation/quote"
+KEY_NAME = "ecdsa_pk"
+KEY_PATH = f"/dev/attestation/keys/{KEY_NAME}"
 
 
-def write_report():
+def generate_key_pair():
+    pass
+
+
+def save_private_key(private_key):
+    with open(KEY_PATH, "w") as f:
+        f.write(private_key)
+
+
+def write_report(report):
     with open(REPORT_PATH, "w") as f:
-        f.write("testreport")
+        f.write(report)
 
 
 def get_quote():
