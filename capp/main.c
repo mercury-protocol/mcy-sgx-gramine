68edc7216daa17a250d27b18d06a199f45e04e0f
mercury <founders@mercuryprotocol.io>
2023-02-11 20:54:32 +0100

    wip

--- capp/main.c
+++ capp/main.c
@@ -1,6 +1,40 @@
+/*
+make
+gramine-sgx ./main
+*/
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(void) {
-    printf("Hello, world\n");
+    FILE* ptr;
+    char ch;
+ 
+    // Opening file in reading mode
+    ptr = fopen("/dev/attestation/quote", "r");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+ 
+    printf("content of this file are \n");
+ 
+    // Printing what is written in file
+    // character by character using loop.
+    do {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+ 
+        // Checking if character is not EOF.
+        // If it is EOF stop reading.
+    } while (ch != EOF);
+
+    printf("content of this file are \n");
+ 
+    // Closing the file
+    fclose(ptr);
     return 0;
 }
