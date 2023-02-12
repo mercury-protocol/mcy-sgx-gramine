79e0349e6a34f6f6089745602cf6598c52d3ac6a
mercury <founders@mercuryprotocol.io>
2023-02-12 17:31:56 +0100

    wip

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-# from ecdsa.curves import SECP256k1
-# from ecdsa import SigningKey
-# from hashlib import sha256
-# import sys
-# sys.path.append("/home/mercury/Documents/repos/mcy-sgx-gramine/venv/lib/python3.8/site-packages")
+import sys
 import os
+
+sys.path.append("/home/mercury/Documents/repos/mcy-sgx-gramine/venv/lib/python3.8/site-packages")
+
+from ecdsa.curves import SECP256k1
+from ecdsa import SigningKey
+from hashlib import sha256
+
 
 REPORT_PATH = "/dev/attestation/user_report_data"
 QUOTE_PATH = "/dev/attestation/quote"
@@ -12,11 +15,11 @@ KEY_PATH = f"/dev/attestation/keys/{KEY_NAME}"
 
 
 def generate_key_pair():
-    # key = SigningKey.generate(curve=SECP256k1, hashfunc=sha256)
-    # public = key.get_verifying_key().to_string()
-    # secret = key.to_string()
-    # return secret, public
-    return bytes("secret", "utf-8"), bytes("public", "utf-8")
+    key = SigningKey.generate(curve=SECP256k1, hashfunc=sha256)
+    public = key.get_verifying_key().to_string()
+    secret = key.to_string()
+    return secret, public
+    # return bytes("1"*16, "utf-8"), bytes("public", "utf-8")
 
 
 def write(path, data):
@@ -25,20 +28,22 @@ def write(path, data):
 
 
 def read(path):
-    with open(path, "r") as f:
+    with open(path, "rb") as f:
         return f.read()
 
 
 def script1():
     sk, pk = generate_key_pair()
-    print(os.listdir("/dev/attestation"))
-    print(os.listdir("/dev/attestation/keys"))
-    print(read("/dev/attestation/quote"))  # PermissionError: [Errno 13] Permission denied: '/dev/attestation/quote'
-                                           # we cannot run python script as root
-    print(read("/dev/attestation/attestation_type"))
 
-    # write_file(KEY_PATH, sk)
-    # write_file("key.txt", sk)
+    print("/: ", os.listdir("/"))
+    print(len(sk))
+    # print("/dev/attestation: ", os.listdir("/dev/attestation"))
+    # print("/dev/attestation/keys: ", os.listdir("/dev/attestation/keys"))
+    # print("/dev/attestation/quote: ", read("/dev/attestation/quote"))
+
+    # write(KEY_PATH, sk)  # PermissionError: [Errno 13] Permission denied: 'key.txt'
+    write("key.txt", sk)  #PermissionError: [Errno 13] Permission denied: 'key.txt'
+    print(read("key.txt"))
 
 
 # write_report()
