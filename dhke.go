e98ad6209145f4aa580e04c07f627e2ece15db67
mercury <founders@mercuryprotocol.io>
2023-08-21 18:48:26 +0200

    shared secret generation is working

--- dhke.go
+++ dhke.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"encoding/hex"
+	"math/big"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,43 +13,52 @@ import (
 
 func main() {
 	// Load the other party's public key from a file
-	remotePubKeyHex, err := ioutil.ReadFile("remote_public_key")
+	remotePubKeyHex, err := ioutil.ReadFile("public")
 	if err != nil {
 		log.Fatal(err)
 	}
+	//fmt.Println("remotePubKeyHex:", string(remotePubKeyHex))
 
 	// Convert the hex public key to bytes
 	remotePubKeyBytes, err := hex.DecodeString(string(remotePubKeyHex))
 	if err != nil {
 		log.Fatal(err)
 	}
+	//fmt.Println("remotePubKeyBytes: ", remotePubKeyBytes)
+	//fmt.Printf("%T\n", remotePubKeyBytes)
 
-	// Unmarshal the remote public key
-	curve := elliptic.P256()
-	remotePubKeyX, remotePubKeyY := elliptic.Unmarshal(curve, remotePubKeyBytes)
+	remotePubKey := createECPublicKey(remotePubKeyBytes)
+	fmt.Println("remotePubKey: ", remotePubKey)
+	fmt.Printf("%T\n", remotePubKey)
 
-	remotePubKey := ecdsa.PublicKey{
-	    Curve: curve,
-	    X:     remotePubKeyX,
-	    Y:     remotePubKeyY,
-	}
+	///////////////////////////////////////////////////////////////6curve
 
-	// Generate your private key
+    // Generate your private key
 	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("privKey: ", privKey)
+    fmt.Printf("%T\n", privKey)
 
-	// Marshal your public key into bytes
-	localPubKeyBytes := elliptic.Marshal(privKey.Curve, privKey.PublicKey.X, privKey.PublicKey.Y)
 
-	// Perform ECDH key exchange
-	sharedX, _ := privKey.Curve.ScalarMult(remotePubKey.X, remotePubKey.Y, privKey.D.Bytes())
-	sharedSecret := sharedX.Bytes()
+    sharedX, sharedY := elliptic.P256().ScalarMult(remotePubKey.X, remotePubKey.Y, privKey.D.Bytes())
+    fmt.Println("sharedX, sharedY: ", sharedX, sharedY)
+    fmt.Printf("%T%T\n", sharedX, sharedY)
 
-	// Print local public key in hex format
-	fmt.Println("Local Public Key (Hex):", hex.EncodeToString(localPubKeyBytes))
+    sharedSecret := new(big.Int).SetBytes(x.Bytes())
+	fmt.Println("sharedSecret:", sharedSecret)
+}
 
-	// Print the shared secret in hex format
-	fmt.Println("Shared Secret (Hex):", hex.EncodeToString(sharedSecret))
+
+func createECPublicKey(pubKeyBytes []byte) *ecdsa.PublicKey {
+	curve := elliptic.P256()
+	x := new(big.Int).SetBytes(pubKeyBytes[:32]) // X coordinate is the first 32 bytes
+	y := new(big.Int).SetBytes(pubKeyBytes[32:]) // Y coordinate is the next 32 bytes
+
+	return &ecdsa.PublicKey{
+		Curve: curve,
+		X:     x,
+		Y:     y,
+	}
 }
