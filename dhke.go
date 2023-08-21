cc5ba06bb1dccdcef9c6e580b3c38ef75d1af26e
mercury <founders@mercuryprotocol.io>
2023-08-21 19:45:41 +0200

    shared secret generation is working

--- dhke.go
+++ dhke.go
@@ -12,58 +12,64 @@ import (
 )
 
 func main() {
-	// Load remote public key from file
-	remotePubKeyHex, err := ioutil.ReadFile("public")
+	remotePubKey := loadRemotePublicKey()
+	fmt.Println("remotePubKey: ", remotePubKey)
+	fmt.Printf("%T\n", remotePubKey)
+
+	privKey := generatePrivateKey()
+	fmt.Println("privKey: ", privKey)
+    fmt.Printf("%T\n", privKey)
+
+    savePublicKey(*privKey)
+
+    sharedSecret := generateSharedSecret(*remotePubKey, *privKey)
+    fmt.Println("sharedSecret:", sharedSecret)
+	fmt.Printf("%T\n", sharedSecret)
+}
+
+
+func loadRemotePublicKey() *ecdsa.PublicKey {
+	remotePubKeyHex, err := ioutil.ReadFile("public_python")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Convert the hex public key to bytes
 	remotePubKeyBytes, err := hex.DecodeString(string(remotePubKeyHex))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	remotePubKey := createECPublicKey(remotePubKeyBytes)
-	fmt.Println("remotePubKey: ", remotePubKey)
-	fmt.Printf("%T\n", remotePubKey)
+	x := new(big.Int).SetBytes(remotePubKeyBytes[:32]) // X coordinate is the first 32 bytes
+	y := new(big.Int).SetBytes(remotePubKeyBytes[32:]) // Y coordinate is the next 32 bytes
 
-	///////////////////////////////////////////////////////////////6curve
+	return &ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     x,
+		Y:     y,
+	}
+}
 
-    // Generate private key
+func savePublicKey(privKey ecdsa.PrivateKey) {
+	pubKeyBytes := append(privKey.PublicKey.X.Bytes(), privKey.PublicKey.Y.Bytes()...)
+	pubKeyHex := hex.EncodeToString(pubKeyBytes)
+
+	err := ioutil.WriteFile("public_go", []byte(pubKeyHex), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generatePrivateKey() *ecdsa.PrivateKey {
 	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("privKey: ", privKey)
-    fmt.Printf("%T\n", privKey)
 
-    // Save the public key in the same format as the Python script
-	pubKeyBytes := append(privKey.PublicKey.X.Bytes(), privKey.PublicKey.Y.Bytes()...)
-	pubKeyHex := hex.EncodeToString(pubKeyBytes)
-	err = ioutil.WriteFile("public_go", []byte(pubKeyHex), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-    // Generate shared secret
-    sharedX, sharedY := elliptic.P256().ScalarMult(remotePubKey.X, remotePubKey.Y, privKey.D.Bytes())
-    fmt.Println("sharedX, sharedY: ", sharedX, sharedY)
-    fmt.Printf("%T%T\n", sharedX, sharedY)
-
-    sharedSecret := new(big.Int).SetBytes(sharedX.Bytes())
-	fmt.Println("sharedSecret:", sharedSecret)
+	return privKey
 }
 
+func generateSharedSecret(remotePubKey ecdsa.PublicKey, privKey ecdsa.PrivateKey) *big.Int {
+    sharedX, _ := elliptic.P256().ScalarMult(remotePubKey.X, remotePubKey.Y, privKey.D.Bytes())
+    sharedSecret := new(big.Int).SetBytes(sharedX.Bytes())
 
-func createECPublicKey(pubKeyBytes []byte) *ecdsa.PublicKey {
-	curve := elliptic.P256()
-	x := new(big.Int).SetBytes(pubKeyBytes[:32]) // X coordinate is the first 32 bytes
-	y := new(big.Int).SetBytes(pubKeyBytes[32:]) // Y coordinate is the next 32 bytes
-
-	return &ecdsa.PublicKey{
-		Curve: curve,
-		X:     x,
-		Y:     y,
-	}
+	return sharedSecret
 }
