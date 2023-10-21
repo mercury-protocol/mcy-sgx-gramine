b36bfb025519385e08b2863f447fc9b9b2d8319c
mercury <founders@mercuryprotocol.io>
2023-10-21 15:08:37 +0200

    move trusted source files to trusted_src forlder and further simplify manifest template

--- app/main.py
+++ app/main.py
@@ -1,35 +1,48 @@
 import os
 
-from constants import LOCAL_PUBLIC_KEY_PATH
-from utils import read
+from trusted_src.constants import LOCAL_PUBLIC_KEY_PATH
 
 
 def startup():
     os.system("make distclean")
     os.system("make")
-    os.system("gramine-sgx ./sgxapp startup.py")
+    os.system("gramine-sgx ./sgxapp trusted_src/startup.py")
 
 
 def get_local_public_key():
-    return read(LOCAL_PUBLIC_KEY_PATH)
+    with open(LOCAL_PUBLIC_KEY_PATH, "r") as file:
+        return file.read()
 
 
 def remote_attestation():
-    os.system("python3 remote_attestation.py")
+    os.system("python3 trusted_src/remote_attestation.py")
 
 
 def verify_attestation():
-    os.system("python3 verify_attestation.py")
+    os.system("python3 trusted_src/verify_attestation.py")
 
 
 def train_model():
-    os.system("gramine-sgx ./sgxapp train_model.py")
+    os.system("gramine-sgx ./sgxapp trusted_src/train_model.py")
+
+
+def is_running_in_docker():
+    try:
+        with open('/proc/1/cgroup', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if 'docker' in line:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
 
 
 if __name__ == "__main__":
     # TODO: these commands shall be performed at docker container startup
-    os.system("/restart_aesm.sh")
-    os.system("gramine-sgx-gen-private-key")
+    if is_running_in_docker():
+        os.system("/restart_aesm.sh")
+        os.system("gramine-sgx-gen-private-key")
 
     startup()
     local_public_key = get_local_public_key()
