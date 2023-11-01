bca9b7672deb8e47b2af00ed453d081f4549d885
mercury <founders@mercuryprotocol.io>
2023-11-01 16:26:16 +0100

    fix insecure cmd args issue

--- app/trusted_src/constants.py
+++ app/trusted_src/constants.py
@@ -6,15 +6,15 @@ QUOTE_PATH = "/dev/attestation/quote"
 LOCAL_SECRET_KEY_PATH = "local_secret_key"
 
 # I/O files
-LOCAL_PUBLIC_KEY_PATH = "local_public_key"
-ATTESTATION_REPORT_PATH = "../attestation_report.json"
-DATA_PUBLIC_KEY_PATH = "data_public_key"
-MODEL_PUBLIC_KEY_PATH = "model_public_key"
-ENCRYPTED_DATA_PATH = "encrypted_data.csv"
-ENCRYPTED_MODEL_PATH = "encrypted_model.py"
-ENCRYPTED_TRAINED_MODEL_PATH = "encrypted_trained_model.pkl"
+LOCAL_PUBLIC_KEY_PATH = "../local_public_key"
+DATA_PUBLIC_KEY_PATH = "../data_public_key"
+MODEL_PUBLIC_KEY_PATH = "../model_public_key"
+ENCRYPTED_DATA_PATH = "../encrypted_data.csv"
+ENCRYPTED_MODEL_PATH = "../encrypted_model.py"
+ENCRYPTED_TRAINED_MODEL_PATH = "../encrypted_trained_model.pkl"
 
 # gramine output files
+ATTESTATION_REPORT_PATH = "../attestation_report.json"
 GR_QUOTE = "../gr.quote"
 IAS_REPORT = "../ias.report"
 IAS_SIGNATURE = "../ias.sig"
