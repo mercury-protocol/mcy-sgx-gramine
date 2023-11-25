2a7da0e33d156b90e5f0bb9f42c13c92893f90f6
peterberekvolgyi <berekvolgyipeter@gmail.com>
2023-11-25 19:02:48 +0100

    add normal pytorch example

---
+++ _pytorch/normal/eval.py
@@ -0,0 +1,43 @@
+import torch
+import matplotlib.pyplot as plt
+
+
+def check_data(test_loader):
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Ground Truth: {}".format(example_targets[i]))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+
+def evaluate_training(train_counter, train_losses, test_counter, test_losses):
+    plt.plot(train_counter, train_losses, color='blue')
+    plt.scatter(test_counter, test_losses, color='red')
+    plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+    plt.xlabel('number of training examples seen')
+    plt.ylabel('negative log likelihood loss')
+    plt.show()
+
+
+def make_predictions(network, test_loader):
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    with torch.no_grad():
+        output = network(example_data)
+
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Prediction: {}".format(
+            output.data.max(1, keepdim=True)[1][i].item()))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
