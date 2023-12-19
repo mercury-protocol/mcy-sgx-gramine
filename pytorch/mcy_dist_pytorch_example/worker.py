03180f6dc49970a52ebd91a7af5df587913bb907
peterberekvolgyi <berekvolgyipeter@gmail.com>
2023-12-19 23:09:16 +0100

    add critical comment

--- pytorch/mcy_dist_pytorch_example/worker.py
+++ pytorch/mcy_dist_pytorch_example/worker.py
@@ -9,6 +9,7 @@ network = ImageClassifierNetwork()
 optimizer = Adam(network.parameters(), lr=1e-3)
 loss_fn = nn.CrossEntropyLoss() 
 
+
 # Training flow 
 if __name__ == "__main__": 
     node_num = parse_node_num()
@@ -18,8 +19,7 @@ if __name__ == "__main__":
 
     dataset = DataLoader(partitioned_dataset, 500)
 
-    for epoch in range(1):  # train for 10 epochs
-        current_batch = 0
+    for epoch in range(1):  # train for n epochs
         for batch in dataset:             
             X, y = batch
             yhat = network(X)
@@ -28,10 +28,10 @@ if __name__ == "__main__":
             # Apply backprop 
             optimizer.zero_grad()
             loss.backward()
+
+            # VERY IMPORTANT: mercury sync code in the user script
             export_gradients(network, node_num)
             wait_for_gradient_updates(network)
-
-            current_batch += 1
 
         print(f"Epoch: {epoch} loss is {loss.item()}")
 
