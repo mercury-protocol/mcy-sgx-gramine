0bffb31e2dd1fbcebca9a62c9cfc7db4373c9b15
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-03-24 19:24:03 +0100

    use run_node function instead of decorator

--- tests/unit/test_pytorch.py
+++ tests/unit/test_pytorch.py
@@ -1,30 +1,26 @@
-from tests.conftest import pytorch_context
 from tests.constants import EXAMPLES_DIR, TEMP_DIR
-from tests.utils import load_model, evaluate_model
+from tests.examples.image_classifier.user_script import create_model
+from tests.utils import run_node, load_model, evaluate_model
 
 
-@pytorch_context(
-    role="WORKER", worker_count=1,
-    temp_dir_name="worker1",
-    example_dir="image_classifier",
-    clear_tmp_dir_end=False
-)
 def test_one_worker_image_classifier():
-    from pytorch.main import main
-    from tests.examples.image_classifier.user_script import create_model
-
-    main()
+    run_node(
+        role="WORKER",
+        worker_count=1,
+        temp_dir_name="worker1",
+        example_dir="image_classifier",
+        clear_tmp_dir_end=False
+    )
 
     model = load_model(TEMP_DIR / "worker1/output", create_model)
     evaluate_model(model, EXAMPLES_DIR / "image_classifier/data")
 
 
-@pytorch_context(
-    role="WORKER-LLM", worker_count=1,
-    temp_dir_name="worker1",
-    example_dir="fine_tune_llm",
-    clear_tmp_dir_end=False
-)
 def test_one_worker_llm():
-    from pytorch.main import main
-    main()
+    run_node(
+        role="WORKER-LLM",
+        worker_count=1,
+        temp_dir_name="worker1",
+        example_dir="fine_tune_llm",
+        clear_tmp_dir_end=False
+    )
