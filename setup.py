ef596d29bbabff36f2d7829bb976ac8ecbbb1d25
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-10-31 18:38:08 +0100

    release version 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def parse_requirements() -> list[str]:
 
 setup(
     name="mcy_dist_ai",
-    version="1.0.0a3",
+    version="1.0.0",
     packages=find_packages(exclude=["tests"]),
     install_requires=parse_requirements(),
     entry_points={
